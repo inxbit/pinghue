0a8b4d2b60984acb2f868b57c4d0f116c5b7af9e
Christian Dassy <18606875+inxbit@users.noreply.github.com>
2026-05-19 16:27:51 -0400

    packaging: update Homebrew formula for v0.2.1
    Update the checked-in Homebrew formula to the published pinghue 0.2.1 PyPI source archive and SHA256.

--- packaging/homebrew/pinghue.rb
+++ packaging/homebrew/pinghue.rb
@@ -10,8 +10,8 @@ class Pinghue < Formula
 
   desc "Colored, concurrent ICMP/TCP ping monitor for the terminal"
   homepage "https://github.com/inxbit/pinghue"
-  url "https://files.pythonhosted.org/packages/0a/5d/6d973413fa30f0c461b1eb5e46a24ce4dbcca8a86534902f2370d0fbab36/pinghue-0.2.0.tar.gz"
-  sha256 "4b09c813cc802d5cff4ee3c1f9e50e314d20f3ec87fe372cb1405f099d62d2a7"
+  url "https://files.pythonhosted.org/packages/3d/94/eda56bfe474bd9a9895137fb83258a7ca0c3de6b0e8f1c0b74930deeaea4/pinghue-0.2.1.tar.gz"
+  sha256 "2291d84f781ab99f5bb4ee9c05e48390cc9a4065987a0735c71395c510a51391"
   license "MIT"
   head "https://github.com/inxbit/pinghue.git", branch: "main"
 
