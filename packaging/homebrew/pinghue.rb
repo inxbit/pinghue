21e164d1beeec20fa3c3f295230c138f8a4dbfdc
Christian Dassy <18606875+inxbit@users.noreply.github.com>
2026-05-25 00:38:52 -0400

    formula: update pinghue to 1.0.2

--- packaging/homebrew/pinghue.rb
+++ packaging/homebrew/pinghue.rb
@@ -10,8 +10,8 @@ class Pinghue < Formula
 
   desc "Colored, concurrent ICMP/TCP ping monitor for the terminal"
   homepage "https://github.com/inxbit/pinghue"
-  url "https://files.pythonhosted.org/packages/de/14/e0458dbd8dc9daee643cbec5eea04aa987151c7fcf0f00784cf918ec8345/pinghue-1.0.1.tar.gz"
-  sha256 "0a186f89b8d1f232cf60628540b18095b541e279f706a4159234ce26d280e493"
+  url "https://files.pythonhosted.org/packages/8b/37/c49145b4da163fd91919672a374fc1f26673242fbc043835ec3a5b21156d/pinghue-1.0.2.tar.gz"
+  sha256 "68efca451656b45c8cacb9be048fcaae663368ea2483ba114e74d1c6b2a54872"
   license "MIT"
   head "https://github.com/inxbit/pinghue.git", branch: "main"
 
